e60661657868ef1df5fbc760a0292a953241c72d
ahmed858 <zez3238@gmail.com>
2023-11-03 00:29:51 +0200

    Create some Models

---
+++ Final Project Solution/Final Project/Models/DomainModels/Patient.cs
@@ -0,0 +1,21 @@
+﻿namespace Final_Project.Models.DomainModels
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        // Address
+        public string Country { get; set; }
+        public string? City { get; set; }
+        public string? Region { get; set; }
+        public string Gender { get; set; }
+        public string Age { get; set; }
+        public ICollection<PatientExamination>? PatientHistroy { get; set; }
+
+
+
+    }
+}
---
+++ Final Project Solution/Final Project/Models/DomainModels/PatientExamination.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Final_Project.Models.DomainModels
+{
+    public class PatientExamination
+
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("Patient")]
+        public int PatientId { get; set;}
+
+        public Patient Patient {  get; set;}
+        public string ExaminationDescribtion { get; set;}
+        
+        [ForeignKey("Doctor")]
+        public int DoctorId { get; set; }
+        public Doctor Doctor { get; set; }
+
+
+
+    }
+}
